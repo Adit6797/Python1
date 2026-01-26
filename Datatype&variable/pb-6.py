e561b306cab7151764b73a6b261e532ebe01710b
Aniket Gupta <ag0655410@gmail.com>
2026-01-26 21:36:38 +0530

    Added python practice files

---
+++ Datatype&variable/pb-6.py
@@ -0,0 +1,5 @@
+a=int(input("Enter the value of a: "))
+
+sqr=a*a
+
+print("The square of a is:",sqr)
---
+++ String/pb-2.py
@@ -0,0 +1,7 @@
+name=input("Enter your name: ")
+date=input("Enter today's date using'/': ")
+
+print("Dear",name,"\n"
+      "You are Selected!!!\n"
+      ,date,
+      "\n ,,,")
